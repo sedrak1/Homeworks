5a2408203db6bf58737afa0dc12a05f4968e7f05
Sedrak Mkrtchyan <setmk777@gmail.com>
2021-04-30 10:48:44 +0400

    fix arr_sort

--- homework3/arr_sort.js
+++ homework3/arr_sort.js
@@ -1,7 +1,9 @@
 function sorting_arr(arr){
     let new_arr = []
     let loop_length = Math.max(...arr) - Math.min(...arr)
-    for(let i = 0; i <= loop_length; i++){
+    
+    
+    for(let i = Math.min(...arr); i <= loop_length + Math.min(...arr); i++){
         if(arr.includes(i)){
             new_arr[i] = i
         }
@@ -9,5 +11,12 @@ function sorting_arr(arr){
             new_arr.push(undefined)
         }    
     }
+
+    //deleting empty items
+    for(let i = 0; i < Math.min(...arr); i++){
+        new_arr.shift()
+    }
+
     console.log(new_arr)
 }
+sorting_arr([0,26, 30, 19, 5])
